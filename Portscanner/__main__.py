5760b96874bcbbca5608e22dabf98c4f429b248a
Your Name <you@example.com>
2024-01-22 08:23:54 +0000

    update

---
+++ Portscanner/__main__.py
@@ -0,0 +1,51 @@
+"""
+This is a basic portscanner
+"""
+
+import socket
+import concurrent.futures
+
+def main():
+    target = input("Enter a target IP: ")
+    start_port = 1
+    end_port = 1000
+
+    def port_scan(target, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((target, port))
+            if result == 0:
+                return port
+            sock.close()
+        except:
+            pass
+        return None
+
+    def scan_ports(target, start_port, end_port):
+        open_ports = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            port_list = range(start_port, end_port +1)
+            results = {executor.submit(port_scan, target, port): port for port in port_list}
+            for future in concurrent.futures.as_completed(results):
+                port = results[future]
+                if future.result():
+                    open_ports.append(port)
+                    print(f"Found open port: {port}")
+        return open_ports
+
+
+    print(f"Scanning ports {start_port}-{end_port} on {target}...")
+
+    open_ports = scan_ports(target, start_port, end_port)
+
+    if open_ports:
+        print("All open ports:")
+        for port in open_ports:
+            print(port)
+    else:
+        print("No open ports found :(")
+
+if __name__ == __main__:
+    main()
+
--- hashcracker/hashcracker.py
+++ hashcracker/hashcracker.py
@@ -4,3 +4,12 @@ import pyfiglet
 BANNER = pyfiglet.figlet_format("HashCracker")
 print(BANNER)
 
+print("Valid hash algorithms: MD5")
+valid_hash_types = ('MD5')
+hash_type = ''
+
+while hash_type not in valid_hash_types:
+    hash_type = imput(str("Enter a hashing algorithm you want to use: "))
+    if hash_type not in valid_hash_types:
+        print("Please enter a valid hashing algorithm.")
+
